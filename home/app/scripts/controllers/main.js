d396967e6de66476a59b1395cb406f45db7fa506
jn23 <tehjawn@gmail.com>
2016-02-28 05:40:02 -0500

    2

--- home/app/scripts/controllers/main.js
+++ home/app/scripts/controllers/main.js
@@ -8,4 +8,21 @@
  */
 angular.module('sbAdminApp')
   .controller('MainCtrl', function($scope,$position) {
+  	$scope.notifications = [
+	  	{
+	  		caller: "Crystal",
+	  		time: "5:58 PM",
+	  		message: "Hi there!"
+	  	},
+	  	{
+	  		caller: "Crystal",
+	  		time: "5:58 PM",
+	  		message: "Hi there!"
+	  	},
+	  	{
+	  		caller: "Crystal",
+	  		time: "5:58 PM",
+	  		message: "Hi there!"
+	  	}
+  	];
   });
