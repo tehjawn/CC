928fe9ee433deeb3532629b8cb6fd1ce019c8393
chethtra yen <chethtra_yen@rocketmail.com>
2016-02-28 08:56:59 -0500

    yay

--- public/talk.js
+++ public/talk.js
@@ -1,4 +1,39 @@
 var caloriesBurn = 0;
+var log = [];
+
+var leanSchedule = [
+{
+ time: "7 am",
+ activity: "Breakfest, egg and wheat bread"
+},
+{
+ time: "9 am",
+ activity: " 30 mintue Run"
+},
+{
+ time: "11 am",
+ activity: "Cycling Club"
+
+},{
+ time: "12 pm",
+ activity: "Lunch, chicken and caesar salad"
+},
+{
+ time: "3 pm",
+ activity: "Lean Weight Training, core and legs"
+},{
+ time: "4 pm",
+ activity: "Snack, fruits"
+},
+{
+ time: "7 pm",
+ activity: "Dinner, salmon with steam vegetables"
+
+
+},{
+ time: "9 am",
+ activity: "Sleep"
+}];
 
 var caloriesRep = function(calories,reps){
         calories = (reps/5)+calories;
@@ -9,8 +44,6 @@ var caloriesTime = function(calories,time){
     calories = (time * 5) + calories;
     return calories;
 }
-
-var log = [];
 
 var name;
 
@@ -43,11 +76,16 @@ $(document).ready(function () {
                           console.log(data);
                           //responsiveVoice.speak("You did " + data.number + data.activity, "UK English Female");
                           //console.log(data.activity);
-                          
+                          var logData = {};
                           switch (data.intent) {
+
                               case 'RepWorkout':
                                   responsiveVoice.speak("Great, I added " + data.number + data.activity + " to your log" , "UK English Female");
                                   caloriesBurn = caloriesRep(caloriesBurn,data.number);
+                                  logData.activity = data.activity;
+                                  logData.count = data.number;
+                                  logData.date = new Date();
+                                  log.push(logData);
                                   break;
                               case 'TimeBaseWorkouts':
                                   var unit; 
@@ -63,7 +101,10 @@ $(document).ready(function () {
                                   }
                                   responsiveVoice.speak("Great, I added " + data.activity + "for" + data.duration.amount +unit+ " to your log" , "UK English Female");
                                   caloriesBurn = caloriesTime(caloriesBurn,data.duration.amount);
-
+                                  logData.activity = data.activity;
+                                  logData.count = data.duration.amount + ' ' + unit;
+                                  logData.date = new Date();
+                                  log.push(logData);
                                   break;
 
                               case 'caloriesCount':
@@ -73,7 +114,15 @@ $(document).ready(function () {
                                    name = data.username;
                                    responsiveVoice.speak("Hello," + name + " welcome to Crystal Coach I am your personal trainer, Crystal", "UK English Female");
                                    break;
-
+                             case 'scheduler':
+                                    var catString = leanSchedule[0].time +" "+ leanSchedule[0].activity +".";
+                                    for(var i = 1; i < leanSchedule.length; i++){
+                                      catString =  catString+leanSchedule[i].time +" "+ leanSchedule[i].activity +".";
+                                    }
+                                    responsiveVoice.speak("Here an overview of your day. " + catString, "UK English Female");
+                               break;
+                               case 'log':
+                               break;
                               default:
                                   responsiveVoice.speak("Sorry I didn't get that" , "UK English Female");
                                   break;
@@ -81,6 +130,7 @@ $(document).ready(function () {
                       }
                   });
               };
+
               recognizer.onerror = function (error) {
                   console.log(error); 
               };
